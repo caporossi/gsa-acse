f932127483a937a0b97cfc14f31fcbc712168348
gcaporossi <gcaporossi@esriitalia.it>
2017-01-24 00:07:49 +0100

    start

--- gsa-acse/Program.cs
+++ gsa-acse/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IniParser;
+using System.IO;
+using IniParser.Model;
 
 namespace gsa_acse
 {
@@ -14,6 +17,22 @@ namespace gsa_acse
         [STAThread]
         static void Main()
         {
+            //Create an instance of a ini file parser
+            FileIniDataParser fileIniData = new FileIniDataParser();
+            // This is a special ini file where we use the '#' character for comment lines
+            // instead of ';' so we need to change the configuration of the parser:
+            fileIniData.Parser.Configuration.CommentString = "#";
+            //Parse the ini file
+            IniData parsedData = fileIniData.ReadFile(@"..\..\Config\gsa-acse.ini");
+            //Write down the contents of the ini file to the console
+            Console.WriteLine("---- Printing contents of the INI file ----\n");
+            Console.WriteLine(parsedData);
+            Console.WriteLine();
+            //Get concrete data from the ini file
+            Console.WriteLine("---- Printing setMaxErrors value from GeneralConfiguration section ----");
+            Console.WriteLine("setMaxErrors = " + parsedData["GeneralConfiguration"]["setMaxErrors"]);
+            Console.WriteLine();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
